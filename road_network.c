84dd90a5abbf7844ed8d89080da44e40673685ed
rafidalazad <141198586+rafidalazad@users.noreply.github.com>
2023-10-21 20:24:27 +1100

    Update road_network.c

--- road_network.c
+++ road_network.c
@@ -1,21 +1,51 @@
 #include "road_network.h"
-#include <stdlib.h>
+#include <stdlib.h>  // For dynamic memory allocation and deallocation
 
-RoadNetwork* initRoadNetwork(int rows, int cols) {
+RoadNetwork* initializeRoadNetwork(int rows, int cols) {
     RoadNetwork* network = (RoadNetwork*)malloc(sizeof(RoadNetwork));
+    if (!network) {
+        // Handle memory allocation failure
+        return NULL;
+    }
+
     network->rows = rows;
     network->cols = cols;
-    network->array = (int**)malloc(rows * sizeof(int*));
-    for (int i = 0; i < rows; i++) {
-        network->array[i] = (int*)malloc(cols * sizeof(int));
+
+    // Allocate memory for the 2D grid
+    network->grid = (int**)malloc(rows * sizeof(int*));
+    if (!network->grid) {
+        // Handle memory allocation failure
+        free(network);
+        return NULL;
     }
+    
+    for (int i = 0; i < rows; i++) {
+        network->grid[i] = (int*)malloc(cols * sizeof(int));
+        if (!network->grid[i]) {
+            // Handle memory allocation failure for a specific row
+            for (int j = 0; j < i; j++) {
+                free(network->grid[j]);
+            }
+            free(network->grid);
+            free(network);
+            return NULL;
+        }
+    }
+
+    // Optionally, initialize the grid with default values or load from some source
+
     return network;
 }
 
 void freeRoadNetwork(RoadNetwork* network) {
+    if (!network) return;
+
+    // Free the 2D grid memory
     for (int i = 0; i < network->rows; i++) {
-        free(network->array[i]);
+        free(network->grid[i]);
     }
-    free(network->array);
+    free(network->grid);
+
+    // Free the network structure itself
     free(network);
 }
