c107ea25c23aec802891683ac5a0e01abd5f2fef
rafidalazad <141198586+rafidalazad@users.noreply.github.com>
2023-10-21 20:44:31 +1100

    Update time.h

--- time.h
+++ time.h
@@ -1,17 +1,48 @@
-#ifndef TIME_H
-#define TIME_H
-
 #include <time.h>
+#include <stdlib.h> // For some general-purpose functions
 
-extern clock_t start_time, end_time;
+#ifdef _WIN32 // Check if on a Windows system
+#include <windows.h>
+#include <psapi.h>
+#else // Assuming a POSIX system (like Linux)
+#include <sys/resource.h>
+#endif
 
-void startTimer();
+// Global variable for performance measurement
+clock_t start_time, end_time;
 
-void stopTimer();
+void startTimer() {
+    start_time = clock();
+}
 
-double getTimeElapsed();
+void stopTimer() {
+    end_time = clock();
+}
 
-int getMemoryUsage();
+double getTimeElapsed() {
+    return ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+}
 
-#endif /* TIME_H */
+int getMemoryUsage() {
+    #ifdef _WIN32 // For Windows
+    PROCESS_MEMORY_COUNTERS pmc;
+    if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
+        return pmc.WorkingSetSize / 1024; // Returning memory in KB
+    }
+    #else // For POSIX
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    return rusage.ru_maxrss; // Returning memory in KB
+    #endif
+
+    return -1; // Error condition, failed to get memory usage
+}
+
+int comparePaths(RoadNetwork* network, Point start, Point end) {
+    int dijkstraWeight = dijkstra(network, start, end);
+    int approxWeight = approximateSolution(network, start, end);
+    return dijkstraWeight - approxWeight;  // Will be 0 if paths are the same weight
+}
+
+
 
