ce4bd6318e8228bf3ea389bf568abb72083a793c
rafidalazad <141198586+rafidalazad@users.noreply.github.com>
2023-10-21 19:57:49 +1100

    Create tests.c

---
+++ tests.c
@@ -0,0 +1,20 @@
+#include "road_network.h"
+#include "approximate_solution.c"
+#include <assert.h>
+
+void testDijkstra() {
+    // Implement white-box and black-box tests for Dijkstra's algorithm.
+    // Use assert statements to check expected vs actual results.
+}
+
+void testApproximateSolution() {
+    // Implement white-box and black-box tests for the approximate solution.
+    // Use assert statements to check expected vs actual results.
+}
+
+int main() {
+    testDijkstra();
+    testApproximateSolution();
+    printf("All tests passed!\n");
+    return 0;
+}
