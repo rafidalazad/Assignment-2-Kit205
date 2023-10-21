ab001f5bcba2d4fa63a4940705b10ec93afcc38b
rafidalazad <141198586+rafidalazad@users.noreply.github.com>
2023-10-21 20:51:52 +1100

    Update approximate_solution.h

--- approximate_solution.h
+++ approximate_solution.h
@@ -1,23 +1,30 @@
 #ifndef APPROXIMATE_SOLUTION_H
 #define APPROXIMATE_SOLUTION_H
 
-// Assuming each point in the grid has x and y coordinates
+// Represents a point in the grid with x and y coordinates
 typedef struct {
     int x;
     int y;
 } Point;
 
-// Structure to represent the road network or 2D grid
+// Represents a node used for pathfinding (like A* algorithm)
 typedef struct {
-    int** grid;         // 2D array representing the grid weights
-    int width;          // Width of the grid
-    int height;         // Height of the grid
+    Point position;         // Position of this node
+    int gCost;              // Cost from the start node
+    int hCost;              // Estimated cost to the end node (heuristic)
+    int fCost;              // Total cost (gCost + hCost)
+    struct Node* parent;    // Pointer to the parent node for path reconstruction
+} Node;
+
+// Represents the road network or 2D grid
+typedef struct {
+    int** grid;             // 2D array representing the grid weights
+    int width;              // Width of the grid
+    int height;             // Height of the grid
 } RoadNetwork;
 
-// Function to compute an approximate path in the road network
-// between the specified start and end points.
-// The function modifies the provided RoadNetwork with '*' indicating the path
-// Returns the total weight/cost of the computed path.
+// Computes an approximate path in the road network between start and end points using A* algorithm.
+// The function returns the total weight/cost of the computed path.
 int approximateSolution(RoadNetwork* network, Point start, Point end);
 
 #endif // APPROXIMATE_SOLUTION_H
