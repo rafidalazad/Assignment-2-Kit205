8427f288092f2dd2c0f605c7bacf20951fd97bce
rafidalazad <141198586+rafidalazad@users.noreply.github.com>
2023-10-21 20:42:48 +1100

    Update approximate_solution.c

--- approximate_solution.c
+++ approximate_solution.c
@@ -18,9 +18,20 @@ double getTimeElapsed() {
 }
 
 // Function to measure memory usage (pseudo code, platform-dependent)
-int getMemoryUsage() {
-    // return the memory used by the process
-    // implementation might vary based on the OS and tools available
+int getMemoryUsage()  
+{
+  #ifdef _WIN32 // For Windows
+    PROCESS_MEMORY_COUNTERS pmc;
+    if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
+        return pmc.WorkingSetSize / 1024; // Returning memory in KB
+    }
+    #else // For POSIX
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    return rusage.ru_maxrss; // Returning memory in KB
+    #endif
+
+    return -1; // Error condition, failed to get memory usage
 }
 
 // Function to compare paths in terms of weights
